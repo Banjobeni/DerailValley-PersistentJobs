9b4f9c2e6fc81ed5be019f2cf3e80c27a15beab2
Banjobeni <none@none>
2023-08-10 23:11:43 +0200

    change EmptyHaulJobWithCarsGenerator to limit number of tracks that can be used for empty hauls

--- PersistentJobsMod/CarSpawningJobGenerators/EmptyHaulJobWithCarsGenerator.cs
+++ PersistentJobsMod/CarSpawningJobGenerators/EmptyHaulJobWithCarsGenerator.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
+using DV.Logic.Job;
 using DV.ThingTypes;
 using PersistentJobsMod.Extensions;
 using PersistentJobsMod.Licensing;
+using UnityEngine;
+using Random = System.Random;
 
 namespace PersistentJobsMod.CarSpawningJobGenerators {
     public static class EmptyHaulJobWithCarsGenerator {
         public static JobChainController TryGenerateJobChain(StationController startingStation, bool requirePlayerLicensesCompatible, Random random) {
-            var yardTracksOrganizer = YardTracksOrganizer.Instance;
-
             var possibleCargoGroupsAndTrainCarCountOrNull = CargoGroupsAndCarCountProvider.GetOrNull(startingStation.proceduralJobsRuleset.outputCargoGroups, startingStation.proceduralJobsRuleset, requirePlayerLicensesCompatible, CargoGroupsAndCarCountProvider.CargoGroupLicenseKind.Cars, random);
 
             if (possibleCargoGroupsAndTrainCarCountOrNull == null) {
@@ -24,11 +25,11 @@ namespace PersistentJobsMod.CarSpawningJobGenerators {
 
             var trainCarLiveries = cargoCarGroups.SelectMany(ccg => ccg.CarLiveries).ToList();
 
-            var requiredTrainLengt = CarSpawner.Instance.GetTotalCarLiveriesLength(trainCarLiveries, true);
+            var requiredTrainLength = CarSpawner.Instance.GetTotalCarLiveriesLength(trainCarLiveries, true);
 
-            var emptyOrAlreadyEmptyHaulTracks = startingStation.logicStation.yard.TransferOutTracks.Where(t => t.GetJobsOfCarsFullyOnTrack().All(j => j.jobType == JobType.EmptyHaul)).ToList();
+            var trackCandidates = GetTrackCandidates(startingStation.logicStation.yard.TransferOutTracks);
 
-            var tracks = yardTracksOrganizer.FilterOutTracksWithoutRequiredFreeSpace(emptyOrAlreadyEmptyHaulTracks, requiredTrainLengt);
+            var tracks = YardTracksOrganizer.Instance.FilterOutTracksWithoutRequiredFreeSpace(trackCandidates, requiredTrainLength);
 
             if (!tracks.Any()) {
                 return null;
@@ -37,6 +38,20 @@ namespace PersistentJobsMod.CarSpawningJobGenerators {
             var track = random.GetRandomElement(tracks);
 
             return EmptyHaulJobProceduralGenerator.GenerateEmptyHaulJobWithCarSpawning(startingStation, track, trainCarLiveries, random);
+        }
+
+        private static List<Track> GetTrackCandidates(List<Track> tracks) {
+            var tracksWithJobs = tracks.Select(t => (Track: t, Jobs: t.GetJobsOfCarsFullyOnTrack())).ToList();
+
+            var alreadyContainingEmptyHaulTracks = tracksWithJobs.Where(t => t.Jobs.Any() && t.Jobs.All(j => j.jobType == JobType.EmptyHaul)).ToList();
+
+            if (alreadyContainingEmptyHaulTracks.Count < Mathf.FloorToInt(0.5f * tracks.Count)) {
+                var noJobsOrAlreadyEmptyHaulTracks = tracksWithJobs.Where(t => t.Jobs.All(j => j.jobType == JobType.EmptyHaul)).Select(t => t.Track).ToList();
+                return noJobsOrAlreadyEmptyHaulTracks;
+            } else {
+                // enough tracks are already occupied with empty hauls. allow to fill up those tracks, but no new ones should be used anymore
+                return alreadyContainingEmptyHaulTracks.Select(t => t.Track).ToList();
+            }
         }
     }
 }
