e37f258bd572a74929ee98e2cc00cf48862c33cf
Banjobeni <none@none>
2024-12-01 17:11:20 +0100

    fix determining required licenses for jobs due to mutability of returned collection type

--- PersistentJobsMod/Licensing/LicensesUtilities.cs
+++ PersistentJobsMod/Licensing/LicensesUtilities.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using DV.ThingTypes;
 
 namespace PersistentJobsMod.Licensing {
     public static class LicensesUtilities {
         public static JobLicenses GetRequiredJobLicenses(JobType jobType, IReadOnlyList<TrainCarType_v2> trainCarTypes, IReadOnlyList<CargoType> cargoTypes, int trainCarCount) {
-            var licenses = LicenseManager.Instance.GetRequiredLicensesForJobType(jobType);
-            
-            licenses.UnionWith(LicenseManager.Instance.GetRequiredLicensesForCarTypes(trainCarTypes));
-            licenses.UnionWith(LicenseManager.Instance.GetRequiredLicensesForCargoTypes(cargoTypes));
+            var licenses = LicenseManager.Instance.GetRequiredLicensesForJobType(jobType).ToList();
+
+            var requiredLicensesForCarTypes = LicenseManager.Instance.GetRequiredLicensesForCarTypes(trainCarTypes);
+            licenses.AddRange(requiredLicensesForCarTypes);
+
+            var requiredLicensesForCargoTypes = LicenseManager.Instance.GetRequiredLicensesForCargoTypes(cargoTypes);
+            licenses.AddRange(requiredLicensesForCargoTypes);
 
             if (LicenseManager.Instance.GetRequiredLicenseForNumberOfTransportedCars(trainCarCount) is var license && license != null) {
                 licenses.Add(license);
