e5eb25bd75c1bc838a873b0f095f43187bbd6878
T3M8029 <102697658+T3M8029@users.noreply.github.com>
2026-01-11 20:46:37 +0100

    Add additional triggers for overall state reset

---
+++ PersistentJobsMod/HarmonyPatches/Save/CarsSaveManager_Patches.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
+using PersistentJobsMod.Persistence;
+using UnityEngine;
+
+namespace PersistentJobsMod.HarmonyPatches.Save
+{
+    /// <summary>patch CarsSaveManager.Load to ensure CarsSaveManager.TracksHash exists</summary>
+    [HarmonyPatch(typeof(CarsSaveManager), "Load")]
+    public static class CarsSaveManager_Patches
+    {
+        public static void Postfix(ref bool __result)
+        {
+            //if no car data is loaded (eg. game update reset them), expire all jobs and allow new cars to re-spawn 
+            if (__result == false)
+            {
+                Debug.Log($"No savegame data found, possibly due to game update. Resetting all jobs and stations.");
+                ResetJobsAndCarsState();
+            }
+            try
+            {
+                var saveData = SaveGameManager.Instance.data.GetJObject(SaveDataConstants.SAVE_DATA_PRIMARY_KEY);
+
+                if (saveData == null)
+                {
+                    Main._modEntry.Logger.Log("Not loading save data: primary object is null.");
+                    return;
+                }
+
+                var spawnBlockSaveData = (JArray)saveData[$"{SaveDataConstants.SAVE_DATA_SPAWN_BLOCK_KEY}#{SaveDataConstants.TRACK_HASH_SAVE_KEY}"];
+                if (spawnBlockSaveData == null)
+                {
+                    Main._modEntry.Logger.Log("Not loading spawn block list: data is null.");
+                }
+                else
+                {
+                    var alreadySpawnedCarsStatioinIds = spawnBlockSaveData.Select(id => (string)id).ToList();
+                    StationIdCarSpawningPersistence.Instance.HandleSavegameLoadedSpawnedStationIds(alreadySpawnedCarsStatioinIds);
+                    Main._modEntry.Logger.Log($"Loaded station spawn block list: [ {string.Join(", ", alreadySpawnedCarsStatioinIds)} ]");
+                }
+            }
+            catch (Exception e)
+            {
+                Main._modEntry.Logger.Warning($"Loading mod data failed with exception:\n{e}");
+            }
+        }
+
+        public static IEnumerator GenerateJobsCurrentStationCoroutine()
+        {
+            while (PlayerManager.PlayerTransform == null)
+            {
+                yield return null;
+            }
+            var station = StationController.allStations.OrderBy(sc => (PlayerManager.PlayerTransform.position - sc.gameObject.transform.position).sqrMagnitude).First();
+            station.ProceduralJobsController.TryToGenerateJobs();
+            StationIdCarSpawningPersistence.Instance.SetHasStationSpawnedCarsFlag(station, true);
+        }
+
+        public static void ResetJobsAndCarsState()
+        {
+            PersistentJobsMod.Console.ExpireAvailableJobsInAllStations();
+            StationIdCarSpawningPersistence.Instance.ClearStationsSpawnedCarsFlagForAllStations();
+            SaveGameManager.Instance.StartCoroutine(GenerateJobsCurrentStationCoroutine());
+        }
+    }
+
+    [HarmonyPatch(typeof(CarsSaveManager), "DeleteAllExistingCars")]
+    public static class CarsSaveManager_DeleteAllExistingCars_Patch
+    {
+        public static void Postfix()
+        {
+            Main._modEntry.Logger.Log($"Savegame data reset, possibly due to mod or game update. Resetting all jobs and stations.");
+            CarsSaveManager_Patches.ResetJobsAndCarsState();
+        }
+    }
+}
---
+++ PersistentJobsMod/HarmonyPatches/Save/SaveGameManager_RemoveData_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistentJobsMod.HarmonyPatches.Save
+{
+    [HarmonyPatch(typeof(SaveGameData), "RemoveData")]
+    public static class SaveGameManager_RemoveData_Patch
+    {
+        public static void Postfix(string _result)
+        {
+            if ((_result == SaveGameKeys.Cars) || (_result == SaveGameKeys.Jobs))
+            {
+                Main._modEntry.Logger.Log($"Savegame data reset, possibly due to mod or game update. Resetting all jobs and stations.");
+                CarsSaveManager_Patches.ResetJobsAndCarsState();
+            }
+        }
+    }
+}
